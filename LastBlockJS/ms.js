33647a0ce60559a3bd4b4cc0a1ac268c748ae8ca
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-02-17 14:04:18 -0300

    Rebuild ms in JS

---
+++ LastBlockJS/ms.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const WebSocket = require('ws')
+
+const config = require('./config/main.json')
+const { WS_IP, WS_PORT } = config
+ 
+const app = express()
+
+// parse application/json
+app.use(express.json())
+
+// current config
+console.log({config})
+
+// Create a WebSocket client
+const ws = new WebSocket(`ws://${config.VM}.${WS_IP}:${WS_PORT}}`);
+
+app.get('/', (_, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/lastblock', (req, res) => {
+  // access to the json payload sent by the client in the request
+  const payload = req.body
+  const { projectid, code } = payload
+
+  console.log(`LB> ID: "${projectid}", Code: "${code}"`)
+
+  // send block data as a websocket client to the websocket server
+  ws.send(JSON.stringify({ projectid, code }))
+
+  // send a response to the client
+  res.send('OK, you are great!')
+})
+
+app.listen(config.PORT, () => {
+  console.log(`\nServer started on port ${config.PORT}]\n`)
+})
